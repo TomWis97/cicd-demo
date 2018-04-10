807853f15273a733b24fb994c43bb82a8c346b71
TomWis97 <github@tomwishaupt.nl>
2018-04-10 09:39:59 +0200

    Fixed oc create setup command

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ oc new-project testing
 oc new-project production
 oc policy add-role-to-user edit system:serviceaccount:cicd-demo:jenkins -n testing
 oc policy add-role-to-user edit system:serviceaccount:cicd-demo:jenkins -n production
-oc create pipelines/development-pipeline.yaml -n cicd-demo
+oc create -f pipelines/development-pipeline.yaml -n cicd-demo
