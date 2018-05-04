a385d7e739d85507852d2aa6a9c6e073edc3ea5c
TomWis97 <github@tomwishaupt.nl>
2018-05-04 15:47:01 +0200

    Fixed permissions within setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -xe
+oc login -u system:admin
 oc import-image httpd --all -n openshift
 oc new-project cicd-demo
 oc new-app jenkins-persistent
@@ -8,3 +9,7 @@ oc new-project production
 oc policy add-role-to-user edit system:serviceaccount:cicd-demo:jenkins -n testing
 oc policy add-role-to-user edit system:serviceaccount:cicd-demo:jenkins -n production
 oc create -f pipelines/development-pipeline.yaml -n cicd-demo
+oc policy add-role-to-user admin admin -n cicd-demo
+oc policy add-role-to-user admin admin -n production 
+oc policy add-role-to-user admin admin -n testing
+
